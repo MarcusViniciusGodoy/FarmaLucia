23629fafb8fddf5eadbed3dda85d2a24d4a6d43e
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-07-29 08:57:59 -0300

    commit inicial

---
+++ backend/src/main/java/com/farmalucia/FarmaLucia/FarmaLuciaApplication.java
@@ -0,0 +1,13 @@
+package com.farmalucia.FarmaLucia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaLuciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaLuciaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/farmalucia/FarmaLucia/FarmaLuciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmalucia.FarmaLucia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaLuciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
