5395b02e2bf24c8dbb5df5ebcb533e90b1a2aa9f
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-07-29 10:38:55 -0300

    adicionando classes e metodos para atendentes

---
+++ backend/src/main/java/com/farmalucia/FarmaLucia/infra/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package com.farmalucia.FarmaLucia.infra;
+
+public class RegraDeNegocioException extends RuntimeException{
+
+    public RegraDeNegocioException(String message){
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/farmalucia/FarmaLucia/infra/repository/AtendenteRepository.java
@@ -0,0 +1,17 @@
+package com.farmalucia.FarmaLucia.infra.repository;
+
+import com.farmalucia.FarmaLucia.infra.entity.Atendente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface AtendenteRepository extends JpaRepository<Atendente, Long> {
+
+    @Query("""
+            SELECT
+                CASE WHEN COUNT(a) > 0 THEN TRUE ELSE FALSE END
+            FROM
+                Atendente a
+            WHERE (a.email = :email) AND (:id IS NULL OR a.id <> :id)
+            """)
+    boolean isJaCadastrado(String email, Long id);
+}
